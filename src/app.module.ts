be14c6c1830af2fa808128fee2247b238167a735
Passakenz <ken.ods.555@gmail.com>
2026-02-20 18:11:39 +0700

    pass

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventsController } from './modules/event/events.controller';
 import { EventsService } from './modules/event/events.service';
+import { ParticipantModule } from './modules/participant/participant.module';
 
 @Module({
-  imports: [],
+  imports: [ParticipantModule],
   controllers: [EventsController],
   providers: [EventsService],
 })
