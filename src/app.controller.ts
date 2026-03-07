05af673179c0c238acd17103498cfa0d968c7dda
Pasakorn Cheeraachanon <68010862@kmitl.ac.th>
2026-03-07 16:52:35 +0700

    add hello page

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common'
+import { ApiExcludeEndpoint, ApiTags } from '@nestjs/swagger'
+
+@ApiTags("start page")
+@Controller()
+export class AppController {
+
+  @Get()
+  getHello(): string {
+    return "hello"
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { EventsController } from './modules/event/events.controller';
 import { EventsService } from './modules/event/events.service';
 import { ParticipantModule } from './modules/participant/participant.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [ParticipantModule],
-  controllers: [EventsController],
+  controllers: [EventsController, AppController],
   providers: [EventsService],
 })
 
