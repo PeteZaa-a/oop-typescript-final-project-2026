cfb9215444c71bb80c7763140fd6d72bfe745a62
Pasakorn Cheeraachanon <68010862@kmitl.ac.th>
2026-03-08 17:00:22 +0700

    add api response

--- src/modules/participant/participant.controller.ts
+++ src/modules/participant/participant.controller.ts
@@ -6,61 +6,65 @@ import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @ApiTags('participants')
 @Controller('participants')
-export class ParticipantController{
-    constructor(private readonly participantService: ParticipantService){}
+export class ParticipantController {
+  constructor(private readonly participantService: ParticipantService) { }
 
-    @Post()
-    @HttpCode(HttpStatus.CREATED)
-    @ApiOperation({ summary: 'สร้างผู้เข้าร่วมใหม่' })
-    async create(@Body() dto: CreateParticipantDto) {
-        return await this.participantService.create(dto);
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'สร้างผู้เข้าร่วมใหม่' })
+  async create(@Body() dto: CreateParticipantDto) {
+    const result = await this.participantService.create(dto)
+    return {
+      success: true,
+      message: "successful to get all data",
+      data: result
     }
+  }
 
-    // @Get()
-    // @ApiOperation({ summary: 'ดึงข้อมูลผู้เข้าร่วมทั้งหมด' })
-    // async findAll() {
-    //     return await this.participantService.findAll();
-    // }
+  // @Get()
+  // @ApiOperation({ summary: 'ดึงข้อมูลผู้เข้าร่วมทั้งหมด' })
+  // async findAll() {
+  //     return await this.participantService.findAll();
+  // }
 
-    @Get(':id')
-    @ApiOperation({ summary: 'ดึงข้อมูลผู้เข้าร่วมตาม ID' })
-    async findOne(@Param('id') id: string) {
-        return await this.participantService.findOne(id);
-    }
+  @Get(':id')
+  @ApiOperation({ summary: 'ดึงข้อมูลผู้เข้าร่วมตาม ID' })
+  async findOne(@Param('id') id: string) {
+    return await this.participantService.findOne(id);
+  }
 
-    @Put(':id')
-    @ApiOperation({ summary: 'อัปเดตข้อมูลผู้เข้าร่วมทั้งหมดตาม ID' })
-    async update(@Param('id') id: string, @Body() dto: CreateParticipantDto) {
-        return await this.participantService.update(id, dto);
-    }   
+  @Put(':id')
+  @ApiOperation({ summary: 'อัปเดตข้อมูลผู้เข้าร่วมทั้งหมดตาม ID' })
+  async update(@Param('id') id: string, @Body() dto: CreateParticipantDto) {
+    return await this.participantService.update(id, dto);
+  }
 
-    @Patch(':id')
-    @ApiOperation({ summary: 'อัปเดตข้อมูลผู้เข้าร่วมบางส่วนตาม ID' })
-    async partialUpdate(@Param('id') id: string, @Body() dto: updateParticipantDto) {
-        return await this.participantService.partialUpdate(id, dto);
-    }
+  @Patch(':id')
+  @ApiOperation({ summary: 'อัปเดตข้อมูลผู้เข้าร่วมบางส่วนตาม ID' })
+  async partialUpdate(@Param('id') id: string, @Body() dto: updateParticipantDto) {
+    return await this.participantService.partialUpdate(id, dto);
+  }
 
-    @Delete(':id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    @ApiOperation({ summary: 'ลบผู้เข้าร่วมตาม ID' })
-    async remove(@Param('id') id: string) {
-        await this.participantService.remove(id);
-    }
-    
-    @Get('events/:eventname')
-    @ApiOperation({ summary: "see people join event"})
-    async getByEvent(@Param("eventname") eventname: string) {
-        return await this.participantService.findParticipantsByEvent(eventname)
-    }
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({ summary: 'ลบผู้เข้าร่วมตาม ID' })
+  async remove(@Param('id') id: string) {
+    await this.participantService.remove(id);
+  }
 
-    @Get()
-    @ApiOperation({ summary: "filter a participant" })
-    async findAll(
-        @Query("event") eventname?: string,
-        @Query("sex") sex?: string,
-        @Query("status") status?: string,
-    ) {
-        return await this.participantService.findWithFilter(eventname, sex, status)
-    }
+  @Get('events/:eventname')
+  @ApiOperation({ summary: "see people join event" })
+  async getByEvent(@Param("eventname") eventname: string) {
+    return await this.participantService.findParticipantsByEvent(eventname)
+  }
+
+  @Get()
+  @ApiOperation({ summary: "filter a participant" })
+  async findAll(
+    @Query("event") eventname?: string,
+    @Query("sex") sex?: string,
+    @Query("status") status?: string,
+  ) {
+    return await this.participantService.findWithFilter(eventname, sex, status)
+  }
 }
-    
