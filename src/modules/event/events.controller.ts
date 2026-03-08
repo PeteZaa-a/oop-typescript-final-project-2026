c21918f570d5d21738878225c3849d4fb3ecbd79
Pasakorn Cheeraachanon <68010862@kmitl.ac.th>
2026-03-08 17:58:33 +0700

    api response in event

--- src/modules/event/events.controller.ts
+++ src/modules/event/events.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Get, Post, Patch, Param, Delete, Put } from "@nestjs/common";
+import { Body, Controller, Get, Post, Patch, Param, Delete, Put, HttpCode, HttpStatus } from "@nestjs/common";
 import { createDtoEvent } from "./dto/create.event.dto"
 import { EventsService } from "./events.service";
 import { UpdateDtoEvent } from "./dto/update.event.dto";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { ApiResponse } from "@/common/interfaces/ApiResponse.Interfaces";
+
 
 @ApiTags("events")
 @Controller("events")
@@ -11,7 +13,8 @@ export class EventsController {
 
   @Get()
   @ApiOperation({ summary: "Get all events" })
-  async findAllEvents() {
+  @HttpCode(HttpStatus.OK)
+  async findAllEvents(): Promise<ApiResponse<createDtoEvent[]>> {
     const getData = await this.eventsService.findAll()
     return {
       success: true,
@@ -22,7 +25,8 @@ export class EventsController {
 
   @ApiOperation({ summary: "create new events" })
   @Post()
-  async createEvent(@Body() createDtoEvent: createDtoEvent) {
+  @HttpCode(HttpStatus.CREATED)
+  async createEvent(@Body() createDtoEvent: createDtoEvent): Promise<ApiResponse<createDtoEvent>> {
     const createData = await this.eventsService.create(createDtoEvent)
     return {
       success: true,
@@ -33,10 +37,11 @@ export class EventsController {
   
   @ApiOperation({ summary: "update events (แก้บางอัน)" })
   @Patch(":eventname")
+  @HttpCode(HttpStatus.OK)
   async update(
     @Param("eventname") eventname: string,
     @Body() updateDto: UpdateDtoEvent
-  ) {
+  ): Promise<ApiResponse<createDtoEvent>> {
     const updateData = await this.eventsService.update(eventname, updateDto)
     return {
       success: true,
@@ -47,10 +52,11 @@ export class EventsController {
 
   @ApiOperation({ summary: "update events (แก้ทั้งหมด)" })
   @Put(":eventname")
+  @HttpCode(HttpStatus.OK)
   async updatAll(
     @Param("eventname") eventname: string,
     @Body() updateDto: createDtoEvent
-  ) {
+  ): Promise<ApiResponse<createDtoEvent>> {
     const result = await this.eventsService.update(eventname, updateDto)
     return {
       success: true,
@@ -61,7 +67,8 @@ export class EventsController {
 
   @ApiOperation({ summary: "delete events" })
   @Delete(":eventname")
-  async remove(@Param("eventname") eventname: string) {
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param("eventname") eventname: string): Promise<ApiResponse<string>> {
     const delEvent = await this.eventsService.remove(eventname)
     return {
       success: true,
@@ -72,7 +79,8 @@ export class EventsController {
 
   @ApiOperation({ summary: "get event by name" })
   @Get(":eventname")
-  async getEventsByName(@Param("eventname") eventname: string) {
+  @HttpCode(HttpStatus.OK)
+  async getEventsByName(@Param("eventname") eventname: string): Promise<ApiResponse<createDtoEvent>> {
     const getEventFromName = await this.eventsService.getEventsByName(eventname)
     return {
       success: true,
